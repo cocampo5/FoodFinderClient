a2abb838b3ad1fbb5469ea0cb41f5e8e9f22bf49
Cristobal Ocampo Quintero <cocampo5@eafit.edu.co>
2016-10-14 21:58:17 -0500

    Primera version

---
+++ app/src/main/java/com/ardilla/foodfinderclient/model/Location.java
@@ -0,0 +1,8 @@
+package com.ardilla.foodfinderclient.model;
+
+/**
+ * Created by Cristo on 27/09/2016.
+ */
+
+public class Location {
+}
---
+++ app/src/main/java/com/ardilla/foodfinderclient/model/Vendedor.java
@@ -0,0 +1,53 @@
+package com.ardilla.foodfinderclient.model;
+
+public class Vendedor {
+    private int id,celular;
+    private String nombre,apellido;
+    private double latitud, longitud;
+
+    public int getId() {return id;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+}
